7a19a79da9a7133044615566e90a01b495b151e7
Calterat <Calterat@gmail.com>
2021-01-23 17:16:51 -0600

    added middleware to server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Server booting up to lend an ear'));
 });
